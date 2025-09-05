7e223c05f7a9f2eeb2b3eb2ae3136610f4bab1e2
udune <udune@naver.com>
2025-09-05 23:16:40 +0900

    init

---
+++ src/main/java/com/example/myownessay/MyownessayApplication.java
@@ -0,0 +1,13 @@
+package com.example.myownessay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyownessayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyownessayApplication.class, args);
+	}
+
+}

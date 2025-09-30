106050dd07640ad970932ce20fb514ed436d94e0
udune <udune@naver.com>
2025-09-30 23:47:45 +0900

    feat: User 일대다 연결, Record 비즈니스 메서드 추가

--- src/main/java/com/example/myownessay/entity/User.java
+++ src/main/java/com/example/myownessay/entity/User.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
@@ -40,6 +41,9 @@ public class User implements UserDetails {
 
     @Column(nullable = false)
     private Boolean isActive = true; // 기본값 설정
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Record> records = new ArrayList<>();
 
     @CreatedDate
     private LocalDateTime createdAt;
